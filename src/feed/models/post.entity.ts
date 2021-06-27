371965f84c7a91741fc9dd41f3b2f5dd57656df4
Mehdi-Benrefad <benrefadmehdi@gmail.com>
2021-06-27 20:10:10 +0100

    first feedPost commit

---
+++ src/feed/models/post.entity.ts
@@ -0,0 +1,14 @@
+import {Entity , Column , PrimaryGeneratedColumn} from 'typeorm';
+
+@Entity('feed_post')
+export class FeedPostEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    body: string;
+
+    @Column({type: 'timestamp' , default: ()=>'CURRENT_TIMESTAMP'})
+    createdAt: Date;
+}
